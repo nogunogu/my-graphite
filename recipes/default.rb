449d876ab1626c0e7c29dc3181d6471fe44f014a
Koichi NOGUCHI <koichi.noguchi@rakuten.com>
2018-11-16 17:34:49 +0900

    install graphite with docker

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,16 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+docker_service 'default' do
+  action [:create, :start]
+end
+
+docker_image 'graphiteapp/graphite-statsd'
+
+docker_container 'graphite' do
+  repo 'graphiteapp/graphite-statsd'
+  container_name 'graphite'
+  restart_policy 'always'
+  port ['80:80', '2003-2004:2003-2004','2023-2024:2023-2024', '8125:8125/udp', '8126:8126']
+end
